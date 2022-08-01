e3e2cfabcb04dabc780b7b0d5ff4b13cec6f6ec7
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-01 15:57:15 +0300

    math operations
    internship

---
+++ Maths/Maths/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2, sum, diff, mux, div;
+            Console.WriteLine("First number: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Second number: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            sum = number1 + number2;
+            diff = number1 - number2;
+            mux = number1 * number2;
+            div = number1 / number2;
+            Console.WriteLine("The sum of these two numbers: " + sum);
+            Console.WriteLine("The difference of these two numbers: " + diff);
+            Console.WriteLine("The multiplication of these two numbers: " + mux);
+            Console.WriteLine("The division of these two numbers: " + div);
+            Console.ReadLine();
+        }
+
+    }
+
+}
